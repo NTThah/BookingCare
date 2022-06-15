1dd1bf9c915da532ad235c3631dee1c11c9f76c6
NguyenThanh <thnh.47@gmail.com>
2022-06-15 09:51:06 +0700

    delete crud

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -28,18 +28,62 @@ let postCRUD = async (req, res) => {
 }
 let displayGetCRUD = async(req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('---------------------')
-    console.log(data);
-    console.log('---------------------')
     return res.render('displayCRUD.ejs',{
         dataTable: data
     });
     // return res.send('Display get crud from homecontroller');
 }
+
+let getEditCRUD = async(req, res) => {
+    let useIid = req.query.id;
+    console.log(req.query.id);
+    if (useIid) {
+        let userData = await CRUDService.getUserInfoById(useIid);
+        // console.log('--------------');
+        // console.log(userData);
+        // console.log('--------------'); 
+        // return res.send('Found a user!');
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+
+    }
+    else {
+        return res.send('User not found');
+    }
+
+}
+
+let putCRUD = async(req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data)
+    return res.render('displayCRUD.ejs',{
+        dataTable: allUsers
+    });
+
+}
+
+let deleteCRUD = async(req, res) => {
+    let id = req.query.id;
+    if (id){
+        let deleteUsers = await CRUDService.deleteUserById(id);
+        return res.send('Delete user success!');
+        // return res.render('displayCRUD.ejs',{
+        //     dataTable: deleteUsers
+        // });
+    }
+    else {
+        return res.send('User not found!');
+    }
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
-    displayGetCRUD: displayGetCRUD
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD:  getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD
 }
--- src/route/web.js
+++ src/route/web.js
@@ -7,8 +7,10 @@ let initWebRouters = (app) => {
     router.get('/about', homeController.getAboutPage);
     router.get('/crud', homeController.getCRUD);
     router.get('/get-crud', homeController.displayGetCRUD);
+    router.get('/edit-crud', homeController.getEditCRUD);
     router.post('/post-crud', homeController.postCRUD);
-    
+    router.post('/put-crud', homeController.putCRUD)
+    router.get('/delete-crud', homeController.deleteCRUD)
 
     return app.use("/", router)
 }
